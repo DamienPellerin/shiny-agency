540f83d4e77dc5878cd12c6050dc8207f1315681
Damien Pellerin <damienpellerin19@gmail.com>
2023-05-12 09:25:29 +0200

    styledComponents

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom'
+import styledComponents from 'styled-components'
+
+const StyledLink = styledComponents(Link)`
+    padding: 15px;
+    color: #8186a0;
+    text-decoration: none;
+    font-size: 18px;
+`
 
 function Header() {
   return (
     <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/survey">Questionnaire</Link>
-      <Link to="/freelances">Freelances</Link>
+      <StyledLink to="/">Accueil</StyledLink>
+      <StyledLink to="/survey">Questionnaire</StyledLink>
+      <StyledLink to="/freelances">Freelances</StyledLink>
     </nav>
   )
 }
