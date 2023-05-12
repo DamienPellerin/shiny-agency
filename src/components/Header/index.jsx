c8938d2a3655791ab962f9d6a70c565f58e74255
Damien Pellerin <damienpellerin19@gmail.com>
2023-05-12 09:48:16 +0200

    Global Style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,24 @@
 import { Link } from 'react-router-dom'
 import styledComponents from 'styled-components'
+import colors from '../../utils/style/colors.js'
 
 const StyledLink = styledComponents(Link)`          
     padding: 15px;
     color: #8186a0;
     text-decoration: none;
     font-size: 18px;
+    ${(props) =>
+      props.$isFullLink &&
+      `color: white; border-radius: 30px; background-color: ${colors.primary};`}
 `
 
 function Header() {
   return (
     <nav>
       <StyledLink to="/">Accueil</StyledLink>
-      <StyledLink to="/survey">Questionnaire</StyledLink>
+      <StyledLink to="/survey" $isFullLink>
+        Questionnaire
+      </StyledLink>
       <StyledLink to="/freelances">Freelances</StyledLink>
     </nav>
   )
