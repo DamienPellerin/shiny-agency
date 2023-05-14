b2827d3993e0e7ac7590e1b9749d885099e384ea
Damien Pellerin <damienpellerin19@gmail.com>
2023-05-14 21:18:12 +0200

    Modification de balise

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,20 +3,20 @@ import styled from 'styled-components'
 import DarkLogo from '/Users/damien/Desktop/shiny-agency/src/assets/dark-logo.png'
 import { StyledLink } from '../../utils/style/atoms'
 
-const NavBar = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 30px;
-`
-
 const HomeLogo = styled.img`
   height: 70px;
 `
 
+const NavContainer = styled.nav`
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
 function Header() {
   return (
-    <NavBar>
+    <NavContainer>
       <Link to="/">
         <HomeLogo src={DarkLogo} />
       </Link>
@@ -25,7 +25,7 @@ function Header() {
       <StyledLink to="/survey/1" $isFullLink>
         Faire le test
       </StyledLink>
-    </NavBar>
+    </NavContainer>
   )
 }
 
