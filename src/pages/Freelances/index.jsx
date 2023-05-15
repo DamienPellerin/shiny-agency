d4fa6a576fde2043bbee93cf96e7976591033c5b
Damien Pellerin <damienpellerin19@gmail.com>
2023-05-15 09:03:11 +0200

    Profils Page

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,12 +1,30 @@
 import DefaultPicture from '../../assets/profil.jpg'
 import Card from '../../components/Card'
 import styled from 'styled-components'
+import colors from '../../utils/style/colors.js'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const CardsContainer = styled.div`
   display: grid;
   gap: 24px;
   grid-template-rows: 350px 350px;
   grid-template-columns: repeat(2, 1fr);
+`
+
+const StyledTitle = styled.h2`
+  padding-top: 30px;
+`
+
+const StyledSubtitle = styled.h3`
+  padding-bottom: 30px;
+  font-size: 15px;
+  line-height: 50px;
+  color: ${colors.secondary};
 `
 
 const freelanceProfiles = [
@@ -29,8 +47,11 @@ const freelanceProfiles = [
 
 function Freelances() {
   return (
-    <div>
-      <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+    <Container>
+      <StyledTitle>Trouvez votre prestataire </StyledTitle>
+      <StyledSubtitle>
+        Chez Shiny nous réunissons les meilleurs profils pour vous.
+      </StyledSubtitle>
       <CardsContainer>
         {freelanceProfiles.map((profile, index) => (
           <Card
@@ -41,7 +62,7 @@ function Freelances() {
           />
         ))}
       </CardsContainer>
-    </div>
+    </Container>
   )
 }
 
