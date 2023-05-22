a515adec0fefcd85f380757b351bbce81a2d0e06
Damien Pellerin <damienpellerin19@gmail.com>
2023-05-22 09:28:13 +0200

    style Survey

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,13 +1,40 @@
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../utils/style/colors.js'
 import { useEffect, useState } from 'react'
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
 
 function Survey() {
   const { questionNumber } = useParams()
   const questionNumberInt = parseInt(questionNumber)
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
   const nextQuestionNumber = questionNumberInt + 1
-  const [questions, setQuestions] = useState([])
+  const [surveyData, setSurveyData] = useState({})
 
   useEffect(() => {
     fetch(`http://localhost:8000/survey`).then((response) =>
@@ -19,16 +46,18 @@ function Survey() {
   }, [])
 
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <h2>Question {questionNumber}</h2>
-      <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-      {questionNumberInt === 10 ? (
-        <Link to="/results">Résultats</Link>
-      ) : (
-        <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
-      )}
-    </div>
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      <QuestionContent>{surveyData[questionNumber]} </QuestionContent>
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
   )
 }
 
